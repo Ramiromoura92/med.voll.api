7398f2978e3eb8c3a3b38337e14d684318fa00ce
Ramiro Silva <ramiromourass@gmail.com>
2025-03-03 20:26:53 -0300

    added dependencies, JPA entity mapping, flyway migration and bean validation.

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,6 +1,30 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String numero, String complemento) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+
+        @NotBlank
+        String logradouro,
+
+        @NotBlank
+        String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+
+        @NotNull
+        String cidade,
+
+        @NotBlank
+        String uf,
+
+        String numero,
+
+        String complemento) {
 
 
 
